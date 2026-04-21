f7bd0c26e79f38b9335ff7636002144efa2cd09d
jihyuuk_home <jihyuk.dev@gmail.com>
2026-04-22 02:32:33 +0900

    refactor: baseURL 변경

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080"
+    //baseURL: "http://localhost:8080"
+    baseURL: "http://3.35.24.95:8080"
 });
 
 export default api;
